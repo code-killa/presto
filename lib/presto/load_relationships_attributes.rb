dc109e49a28d3c045a69ba6a935ef643c8eae15c
Luis J. Rodriguez <ljrzarate@gmail.com>
2015-10-27 14:56:11 -0500

    refactor LoadRelationshipsAttributes, now loding parameter and relationships dynamically

--- lib/presto/load_relationships_attributes.rb
+++ lib/presto/load_relationships_attributes.rb
@@ -7,51 +7,30 @@ module Presto
 
     def klass_relationships
       @klass.reflect_on_all_associations.each do |relationship|
-        case relationship.macro.to_s
-        when "has_many"
-          has_many_attributes(relationship) if Presto::load_has_many_relationships
-        when "has_one"
-          has_one_attributes(relationship) if Presto::load_has_one_relationships
-        end
+        create_params_for_association(relationship)
       end
       @resp
     end
 
-    private
-      def has_many_attributes(relationship)
-        if relationship.options.has_key? :through
-          has_many_through_attributes(relationship)
+    def create_params_for_association(relationship)
+      self.class.send :define_method, "#{relationship.macro}_attributes".to_sym do |relationship|
+        attributes = relationship.klass.column_names.map{ |column| column.to_sym }
+        if relationship.options.any? && relationship.options.has_key?(:through)
+          self.send("#{relationship}_through_attributes".to_sym, relationship, attributes)
         else
-          has_many_basic_attributes(relationship)
+          self.send("#{relationship}_basic_attributes".to_sym, relationship, attributes)
         end
       end
 
-      def has_many_basic_attributes(relationship)
-        attributes = relationship.klass.column_names.map{ |column| column.to_sym }
+      self.class.send :define_method, "#{relationship}_through_attributes".to_sym do |relationship, attributes|
+        @resp["#{relationship.options[:through]}_attributes".to_sym] << {"#{relationship.plural_name}_attributes": attributes}
+      end
+
+      self.class.send :define_method, "#{relationship}_basic_attributes".to_sym do |relationship, attributes|
         @resp["#{relationship.plural_name}_attributes".to_sym] = attributes
       end
 
-      def has_many_through_attributes(relationship)
-        attributes = relationship.klass.column_names.map{ |column| column.to_sym }
-        @resp["#{relationship.options[:through]}_attributes".to_sym] << {"#{relationship.plural_name}_attributes": attributes}
-      end
-
-      def has_one_attributes(relationship)
-        if relationship.options.has_key? :through
-          has_one_through_attributes(relationship)
-        else
-          has_one_basic_attributes(relationship)
-        end
-      end
-
-      def has_one_basic_attributes(relationship)
-        attributes = relationship.klass.column_names.map{ |column| column.to_sym }
-        @resp["#{relationship.name}_attributes".to_sym] = attributes
-      end
-
-      def has_one_through_attributes(relationship)
-        attributes = relationship.klass.column_names.map{ |column| column.to_sym }
-        @resp["#{relationship.options[:through]}_attributes".to_sym] << {"#{relationship.plural_name}_attributes": attributes}
-      end
+      self.send("#{relationship.macro}_attributes".to_sym, relationship)
+    end
   end
 end
