11989053420d468a85f0b85896172d0b72fcf5b7
luisk Hernandez <luisk.hernandez.macias@gmail.com>
2015-11-01 20:30:34 -0500

    Adding link to the relationship in the index view

--- app/helpers/presto/items_helper.rb
+++ app/helpers/presto/items_helper.rb
@@ -1,4 +1,14 @@
 module Presto
   module ItemsHelper
+    def th_item(colum_name)
+      return colum_name unless colum_name.include?("_id")
+      colum_name.gsub("_id","")
+    end
+
+    def td_item(model,name)
+      return model.send(name.to_sym) unless name.include?("_id")
+      val= model.send(name.to_sym)
+      link_to val, edit_item_path(name.gsub("_id",""), val)
+    end
   end
 end
