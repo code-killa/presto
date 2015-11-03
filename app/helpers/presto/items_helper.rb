361d0824ba2428fc0897f091258f7d230b679c75
luisk Hernandez <luisk.hernandez.macias@gmail.com>
2015-11-02 23:50:44 -0500

    Adding Search in the index view
    By default it search only in all the string and text fields

--- app/helpers/presto/items_helper.rb
+++ app/helpers/presto/items_helper.rb
@@ -4,6 +4,9 @@ module Presto
        Presto::nested_attributes
     end
 
+    def search_text_field_sym(klass)
+      (klass.column_types.to_a.map { |type| type[0] if string_or_text?(type)  }.compact.join("_or_") + "_cont").to_sym
+    end
     def th_item(q,colum_name)
       return sort_link(q,colum_name) unless colum_name.include?("_id")
       sort_link(q,colum_name,colum_name.gsub("_id",""))
@@ -14,5 +17,10 @@ module Presto
       val= model.send(name.to_sym)
       link_to val, edit_item_path(name.gsub("_id",""), val)
     end
+    private
+     def string_or_text?(column_type)
+       val = column_type[1].class.to_s.split("::").last
+       (val == "String" || val == "Text")
+     end
   end
 end
